2c8c545999ec82d6ea4b2cc7856a5770f1bc94ef
Maxine Ellah <email": null>
2016-05-04 10:57:51 +1200

    install additional modules

---
+++ migrations/20160504103414_createWordsTable.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTableIfNotExists('words', function(table){
+       table.increments('id')
+       table.string('word')
+       table.string('image_url')
+       table.string('sound_url')
+       console.log('words table was created')
+     })
+ };
+
+ exports.down = function(knex, Promise) {
+   return knex.schema.dropTableIfExists('words').then(function(){
+     console.log('words table was dropped')
+   })
+ };
---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var app = express();
+var path = require('path')
+var bodyparser = require('body-parser')
+var dotenv = require('dotenv')
+var $ = require('jquery')
+var cloudinary = require('cloudinary')
