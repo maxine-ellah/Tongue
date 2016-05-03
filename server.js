45a8c19047100c6ccc8e06849c665e3085d40f53
Maxine Ellah <email": null>
2016-05-04 11:29:02 +1200

    make app.js

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 var express = require('express');
 var app = express();
 var path = require('path')
-var bodyparser = require('body-parser')
+var bodyParser = require('body-parser')
 var dotenv = require('dotenv')
 var $ = require('jquery')
 var cloudinary = require('cloudinary')
+var fileUpload = require('express-fileupload');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(fileUpload());
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
