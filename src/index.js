e5147122b841e84ed1b9b9061f61bf0528893b36
Maxine Ellah <email": null>
2016-05-05 16:35:55 +1200

    replace data in view rather than append new view each time

--- src/index.js
+++ src/index.js
@@ -5,22 +5,30 @@ var request = require('superagent')
 // $('document').ready(function(){
 //   $('#content').append(singlePage({ word: res.body }))
 // })
+var firstTime = true
 
 $('button#nextWord').click(function() {
 
   request
     .get('http://localhost:3000/words')
     .end(function(err, res) {
-    var viewWithData =  singlePage({ words: JSON.parse(res.text) })
-    var firstTime = true
+      var viewWithData = singlePage({ words: JSON.parse(res.text) })
+
       if (firstTime) {
-      $('#content').append(viewWithData)
-       firstTime = false
-     } else {
-       $('#content').html(viewWithData)
-     }
+        console.log("i made it here!")
+        $('#content').append(viewWithData)
+        firstTime = false
+      } else {
+        $('#content').html(viewWithData)
+      }
 
     })
-    document.getElementById("#content").reset()
-    return false
+    // document.getElementById("#content").reset()
+    // return false
 })
+
+
+// assumed :
+// 1. we've reaved line 18
+// 2. the viewWithData is correct
+// 3. the append did / didn't happen?
