c997615073b99ea5b11905286c403e84fc348cd8
Maxine Ellah <email": null>
2016-05-10 10:05:48 +1200

    uncomment lines in index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ var request = require('superagent')
 
 // $('document').ready(function(){
 
+// try adding request to handle
+  // request
+  //   .get('http://localhost:3000/auth/facebook/callback')
+  //   .end(function(err, res) {
+  //
+  //   })
+
+
+
+
 var firstTime = true
 
 $('button#nextWord').click(function() {
